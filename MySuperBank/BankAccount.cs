4d6efd7e91b3718392c516129d58cf6f93849130
IanJosephK1 <ian@loopreality.com>
2023-07-12 11:37:16 +0530

    OOP CSharp

---
+++ MySuperBank/BankAccount.cs
@@ -0,0 +1,61 @@
+﻿namespace MySuperBank
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+
+        public decimal Balance { get
+            {
+                decimal balance = 0;
+                foreach (var transaction in allTransactions)
+                {
+                    balance += transaction.Amount;
+                    
+                }
+                return balance;
+            }
+        }
+
+        private static int accountNumberSeed = 1234567890;
+
+        List<Transaction> allTransactions = new List<Transaction>();
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException(nameof(amount), "Amount must be positive");
+            }
+            Transaction depositTransaction = new Transaction(amount, date, note);
+            allTransactions.Add(depositTransaction);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException(nameof(amount), "Amount must be positive");
+            }
+
+            if(Balance - amount < 0)
+            {
+                throw new InvalidOperationException( "You do not have sufficient funds to make this transaction");
+            }
+
+            Transaction withdrwalTransaction = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrwalTransaction);
+        }
+
+        public BankAccount(string name)
+        {
+            this.Owner = name;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+            MakeDeposit(10000, DateTime.Now, "Kendra");
+
+
+        }
+    }
+}
---
+++ MySuperBank/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MySuperBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount("Kendra");
+            Console.WriteLine($"A new account for {bankAccount.Owner} was created, they have ${bankAccount.Balance}");
+        }
+    }
+}
